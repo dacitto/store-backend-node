62b09e8cf889534185d9e98d3971542668e4c16f
Salah daci <dacidigital@gmail.com>
2023-01-18 17:44:07 +0100

    feat: add home and 404 pages

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import db from './database'
+import path from 'path'
 // import routes from "./routes";
 import appConf from './config/app.config'
+import { logger } from './middleware/logger'
+import { errorHandler } from './middleware/errorHandler'
+import rootRouter from './routes/root'
 
 // ! test data base connection
 try {
@@ -16,13 +20,30 @@ const app: Application = express()
 const port = appConf.port || 3000
 
 // Defaul middlewares
+app.use(logger)
 app.use(cors(), express.json())
 
 // Routes
+app.use(express.static('public'))
+app.use('/', rootRouter)
 // app.use("/api", routes);
 app.get('/', function (req: Request, res: Response) {
-  res.send('Hello Worlsd!')
+  res.send('Hello World!')
 })
+// Not found pages
+app.all('*', (req: Request, res: Response) => {
+  res.status(404)
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, '../public/views/404.html'))
+  } else if (req.accepts('json')) {
+    res.json({ message: '404 Not Found' })
+  } else {
+    res.type('txt').send('404 Not Found')
+  }
+})
+
+app.use(errorHandler)
+
 // Start the server
 app.listen(port, () => {
   console.log(`${appConf.name} Server is running on port: ${port}`)
