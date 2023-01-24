3387c6642a5ebcc8679dcc5162064ef01dc3bf4e
Salah daci <dacidigital@gmail.com>
2023-01-24 11:49:58 +0100

    test: add auth tests

---
+++ src/tests/routes/auth.router.spec.ts
@@ -0,0 +1,33 @@
+import { generateUsername } from 'unique-username-generator'
+import supertest from 'supertest'
+import app from '../../server'
+import { User } from '../../models/user.model'
+const request = supertest(app)
+
+const newUser: Omit<User, 'id'> = {
+  username: generateUsername('', 0, 15),
+  first_name: 'Med',
+  last_name: 'Daci',
+  password: '123@Test',
+}
+
+describe('Test Auth endpoints responses', () => {
+  it('Unauthorized', async () => {
+    const response = await request.get('/api/users')
+    expect(response.status).toBe(401) // Unauthorized
+  })
+
+  it('auth/login', async () => {
+    await request
+      .post('/api/auth/login')
+      .send({ username: 'user1', password: '123@Test' })
+      .expect(200) // Ok
+  })
+  it('api/auth/register', async () => {
+    await request
+      .post('/api/auth/register')
+      .set('Accept', 'application/json')
+      .send(newUser)
+      .expect(201) // created
+  })
+})
